b261917abc8e4a144af136454db9e989d5abf3cd
David Tchepak <dave@davesquared.net>
2013-08-12 22:14:47 +1000

    Rename ReaderOption to Possible and add examples

--- src/XSharpx/Possible.cs
+++ src/XSharpx/Possible.cs
@@ -1,30 +1,30 @@
 ﻿using System;
 
 namespace XSharpx {
-  public struct ReaderOption<R, A> {
+  public struct Possible<R, A> {
     private readonly Func<R, Option<A>> runReader;
 
-    public ReaderOption(Func<R, Option<A>> runReader) { this.runReader = runReader; }
+    public Possible(Func<R, Option<A>> runReader) { this.runReader = runReader; }
 
     public Option<A> Run(R r) { return runReader(r); }
 
-    public ReaderOption<R, B> Select<B>(Func<A, B> f) {
+    public Possible<R, B> Select<B>(Func<A, B> f) {
       var self = this;
-      return new ReaderOption<R, B>(r => self.Run(r).Select(f));
+      return new Possible<R, B>(r => self.Run(r).Select(f));
     }
 
-    public ReaderOption<R, B> SelectMany<B>(Func<A, ReaderOption<R, B>> f) {
+    public Possible<R, B> SelectMany<B>(Func<A, Possible<R, B>> f) {
       return SelectMany(f, (a, b) => b);
     }
 
-    public ReaderOption<R, C> SelectMany<B, C>(Func<A, ReaderOption<R, B>> f, Func<A, B, C> selector) {
+    public Possible<R, C> SelectMany<B, C>(Func<A, Possible<R, B>> f, Func<A, B, C> selector) {
       var self = this;
       Func<R, Option<C>> func = r => {
         var maybeA = self.Run(r);
         var maybeB = maybeA.SelectMany(a => f(a).Run(r));
         return maybeA.Select(a => maybeB.Select(b => selector(a, b))).Flatten();
       };
-      return new ReaderOption<R, C>(func);
+      return new Possible<R, C>(func);
     }
   }
 }
